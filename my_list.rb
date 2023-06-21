947026250e42f6e9a9bcd007841e96b18ae6b72e
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-21 15:33:33 -0400

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -17,7 +17,5 @@ list = MyList.new([1, 2, 3, 4])
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 }
-
-
+puts(list.any? { |e| e == 5 })
 puts list.filter(&:even?).inspect
