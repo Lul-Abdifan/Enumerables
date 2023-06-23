ca74119eb8aff24cca4e9d840703b811fa8ea8e5
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-23 02:19:37 +0000

    Add &block syntax for consistency and improved readability.

--- enumarable.rb
+++ enumarable.rb
@@ -1,18 +1,18 @@
 # Define a module MyEnumerable
 module MyEnumerable
-  def all?
+  def all?(&block)
      # Check if all elements in the list satisfy the condition specified by the block
     @list.each { |elt| return false unless yield(elt) }
     true
   end
 
-  def any?
+  def any?(&block)
      # Check if any element in the list satisfies the condition specified by the block
     @list.each { |elt| return true if yield(elt) }
     false
   end
 
-  def filter?
+  def filter?(&block)
      # Filter the list and store elements that satisfy the condition specified by the block
     filter_store = []
     @list.each do |elt|
