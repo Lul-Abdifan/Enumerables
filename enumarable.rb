33f58763cd2d6537d162a143c7eb73367bf60b55
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-23 02:30:57 +0000

    Fix linters error

--- enumarable.rb
+++ enumarable.rb
@@ -1,19 +1,19 @@
 # Define a module MyEnumerable
 module MyEnumerable
-  def all?(&block)
-     # Check if all elements in the list satisfy the condition specified by the block
+  def all?
+    # Check if all elements in the list satisfy the condition specified by the block
     @list.each { |elt| return false unless yield(elt) }
     true
   end
 
-  def any?(&block)
-     # Check if any element in the list satisfies the condition specified by the block
+  def any?
+    # Check if any element in the list satisfies the condition specified by the block
     @list.each { |elt| return true if yield(elt) }
     false
   end
 
-  def filter?(&block)
-     # Filter the list and store elements that satisfy the condition specified by the block
+  def filter?
+    # Filter the list and store elements that satisfy the condition specified by the block
     filter_store = []
     @list.each do |elt|
       filter_store << elt if yield(elt)
